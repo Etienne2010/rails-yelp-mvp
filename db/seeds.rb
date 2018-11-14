b63dadf55663cd10b6ef494780655ac705d725fb
Etienne Lioret <kavatine99@gmail.com>
2018-11-14 06:20:51 -0500

    Journee 26, exercice 2 fini

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+5.times do
+  r = Restaurant.new(name: Faker::HarryPotter.character, address: Faker::Address.full_address, category: ["chinese", "italian", "japanese", "french", "belgian"].sample(1)[0])
+  if (r.save)
+    puts "saved, #{r.name}"
+  else
+    puts "not saved, #{r.errors}"
+    puts "#{r.name}, #{r.address}, #{r.category}"
+  end
+end
